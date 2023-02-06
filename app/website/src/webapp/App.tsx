de68e15f17e290e37b6d1dca9bdcecbadd88f96a
Joe McCormick <joe.c.mccormick@gmail.com>
2023-02-05 19:09:38 -0800

    default theme from media query

--- app/website/src/webapp/App.tsx
+++ app/website/src/webapp/App.tsx
@@ -4,18 +4,20 @@ import React, { Suspense, useEffect, useState } from 'react';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import deepmerge from '@mui/utils/deepmerge';
 import createTheme from '@mui/material/styles/createTheme';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { useRedux, useComponents } from 'awayto-hooks';
 
 import { getBaseComponents, getDesignTokens, getThemedComponents } from './style';
 
-// import './App.css';
+import './App.css';
 
-const App = (props: Partial<IProps>): JSX.Element => {
+const App = (props: IProps): JSX.Element => {
 
   const { Layout } = useComponents();
   const { theme } = useRedux(state => state.util);
 
-  const currentTheme = React.useMemo(() => createTheme(deepmerge(deepmerge(getDesignTokens(theme || 'dark'), getThemedComponents(theme || 'dark')), getBaseComponents())), [theme]);
+  const defaultTheme = useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light';
+  const currentTheme = React.useMemo(() => createTheme(deepmerge(deepmerge(getDesignTokens(theme || defaultTheme), getThemedComponents(theme || defaultTheme)), getBaseComponents())), [theme]);
 
   return <>
     <Suspense>
