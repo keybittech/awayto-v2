2e55ba9725e29fdf78553324649e8cb62aca13f4
Joe McCormick <joe.c.mccormick@gmail.com>
2023-04-07 05:27:59 -0700

    Modify test.js as per instructions: binary search tree implementation in javascript

---
+++ test.js
@@ -0,0 +1,22 @@
+// Binary Search Tree Implementation in JavaScript
+
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function(node) {
+        if (data < node.data) {
