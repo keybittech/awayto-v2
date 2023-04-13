bfe6eeeaeba2d9829d05e39e757155ac2ae2d866
Joe McCormick <joe.c.mccormick@gmail.com>
2023-04-12 23:24:52 -0700

    ChatJOEPT - add a function somewhere in the file n

--- core/types/assist.ts
+++ core/types/assist.ts
@@ -1,24 +1,34 @@
-import { Extend } from '../util';
-import { ApiHandler, ApiOptions, EndpointType, siteApiHandlerRef, siteApiRef } from './api';
+function greet() {
+  alert("hello");
+}
+
+import { Extend } from "../util";
+import {
+  ApiHandler,
+  ApiOptions,
+  EndpointType,
+  siteApiHandlerRef,
+  siteApiRef,
+} from "./api";
 
 /**
  * @category Assist
  */
 export enum IPrompts {
-  CREATE_API = 'create_api',
-  CREATE_API_BACKEND = 'create_api_backend',
-  CREATE_TYPE = 'create_type',
-  CREATE_COMPONENT = 'create_component',
-  EDIT_FILE = 'edit_file',
-  MORPH_FILE = 'morph_file',
-  MORPH_2 = 'morph_2',
-  MORPH_3 = 'morph_3',
-  MIRROR_EDIT = 'mirror_edit',
-  SUGGEST_ROLE = 'suggest_role',
-  SUGGEST_SERVICE = 'suggest_service',
-  SUGGEST_TIER = 'suggest_tier',
-  SUGGEST_FEATURE = 'suggest_feature',
-  CONVERT_PURPOSE = 'convert_purpose'
+  CREATE_API = "create_api",
+  CREATE_API_BACKEND = "create_api_backend",
+  CREATE_TYPE = "create_type",
+  CREATE_COMPONENT = "create_component",
+  EDIT_FILE = "edit_file",
+  MORPH_FILE = "morph_file",
+  MORPH_2 = "morph_2",
+  MORPH_3 = "morph_3",
+  MIRROR_EDIT = "mirror_edit",
+  SUGGEST_ROLE = "suggest_role",
+  SUGGEST_SERVICE = "suggest_service",
+  SUGGEST_TIER = "suggest_tier",
+  SUGGEST_FEATURE = "suggest_feature",
+  CONVERT_PURPOSE = "convert_purpose",
 }
 
 /**
@@ -37,14 +47,14 @@ export type IAssist = {
 const assistApi = {
   getPrompt: {
     kind: EndpointType.QUERY,
-    url: 'assist/prompt?id=:id&prompt=:prompt',
-    method: 'GET',
+    url: "assist/prompt?id=:id&prompt=:prompt",
+    method: "GET",
     opts: { cache: null } as ApiOptions,
     queryArg: {
-      id: '' as string,
-      prompt: '' as string
+      id: "" as string,
+      prompt: "" as string,
     },
-    resultType: { promptResult: [] as string[] }
+    resultType: { promptResult: [] as string[] },
   },
 } as const;
 
@@ -52,12 +62,17 @@ const assistApi = {
  * @category Assist
  */
 const assistApiHandlers: ApiHandler<typeof assistApi> = {
-  getPrompt: async props => {
+  getPrompt: async (props) => {
     const { id, prompt } = props.event.queryParameters;
-    const generatedPrompt = props.completions.generatePrompt(id as IPrompts, ...prompt.split('|'));
-    const promptResult = await props.completions.getChatCompletionPrompt(generatedPrompt);
+    const generatedPrompt = props.completions.generatePrompt(
+      id as IPrompts,
+      ...prompt.split("|")
+    );
+    const promptResult = await props.completions.getChatCompletionPrompt(
+      generatedPrompt
+    );
     // return { promptResult: promptResult[0].text?.trim().replace(/\r?\n|\r/g, '').split('|').filter(a => !!a) };
-    return { promptResult: promptResult.split('|').filter(a => !!a) };
+    return { promptResult: promptResult.split("|").filter((a) => !!a) };
   },
 } as const;
 
@@ -71,9 +86,9 @@ type AssistApi = typeof assistApi;
  */
 type AssistApiHandler = typeof assistApiHandlers;
 
-declare module './api' {
-  interface SiteApiRef extends Extend<AssistApi> { }
-  interface SiteApiHandlerRef extends Extend<AssistApiHandler> { }
+declare module "./api" {
+  interface SiteApiRef extends Extend<AssistApi> {}
+  interface SiteApiHandlerRef extends Extend<AssistApiHandler> {}
 }
 
 Object.assign(siteApiRef, assistApi);
