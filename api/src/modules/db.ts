885f55f82eac2dca98936fb2d2cd889e334ca285
Joe McCormick <joe.c.mccormick@gmail.com>
2023-04-13 03:14:02 -0700

    ChatJOEPT - implement an addition function that ad

--- api/src/modules/db.ts
+++ api/src/modules/db.ts
@@ -1,69 +1,65 @@
-import { v4 as uuid } from 'uuid';
-import redis from './redis';
-import { IUserProfile } from 'awayto/core';
-import pgPromise from 'pg-promise';
+function add(a: number, b: number): number {
+  return a + b;
+}
+import redis from "./redis";
+import { IUserProfile } from "awayto/core";
+import pgPromise from "pg-promise";
 
-const {
-  PG_HOST,
-  PG_PORT,
-  PG_USER,
-  PG_PASSWORD,
-  PG_DATABASE
-} = process.env as { [prop: string]: string } & { PG_PORT: number };
+const { PG_HOST, PG_PORT, PG_USER, PG_PASSWORD, PG_DATABASE } = process.env as {
+  [prop: string]: string;
+} & { PG_PORT: number };
 
 export let connected: boolean = false;
 
 const pgp = pgPromise();
 
 const pgTypes = pgp.pg.types;
-pgTypes.setTypeParser(1114, stringValue => stringValue);
-pgTypes.setTypeParser(1082, stringValue => stringValue);
+pgTypes.setTypeParser(1114, (stringValue) => stringValue);
+pgTypes.setTypeParser(1082, (stringValue) => stringValue);
 
 export const db = pgp({
   host: PG_HOST,
   port: PG_PORT,
   user: PG_USER,
   password: PG_PASSWORD,
-  database: PG_DATABASE
-})
+  database: PG_DATABASE,
+});
 // export let db: Client = new postgres.Client();
 
 async function go() {
-
   try {
-
     while (false === redis.isReady) {
-      console.error('redis is not ready');
-      await new Promise<void>(res => setTimeout(() => res(), 250))
+      console.error("redis is not ready");
+      await new Promise<void>((res) => setTimeout(() => res(), 250));
     }
 
     await db.connect();
-    console.log('db connected');
+    console.log("db connected");
 
     try {
       // Set admin sub
-      const { sub }= await db.one<IUserProfile>(`
+      const { sub } = await db.one<IUserProfile>(`
         SELECT sub
         FROM dbtable_schema.users
         WHERE username = 'system_owner'
       `);
-      await redis.set('adminSub', sub);
+      await redis.set("adminSub", sub);
     } catch (error) {
       const sub = uuid();
-      await db.none(`
+      await db.none(
+        `
         INSERT INTO dbtable_schema.users (sub, username, created_on, created_sub)
         VALUES ($1::uuid, $2, $3, $1::uuid)
-      `, [sub, 'system_owner', new Date()]);
+      `,
+        [sub, "system_owner", new Date()]
+      );
 
-      await redis.set('adminSub', sub);
+      await redis.set("adminSub", sub);
     }
 
     connected = true;
-
   } catch (error) {
-
-    console.log({ PGCONNECTERROR: error })
-
+    console.log({ PGCONNECTERROR: error });
   }
 }
 
