ae66771665e42c9fc69bf0630a18f39c085b2ca9
Joe McCormick <joe.c.mccormick@gmail.com>
2023-05-24 21:54:05 -0700

    impl clickable group info

--- app/website/src/modules/groups/ManageGroupHome.tsx
+++ app/website/src/modules/groups/ManageGroupHome.tsx
@@ -1,19 +1,32 @@
-import React, { useMemo } from 'react';
+import React, { Suspense, useContext, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router';
 
+import Tooltip from '@mui/material/Tooltip';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
-import { SiteRoles} from 'awayto/core';
-import { useComponents } from 'awayto/hooks';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+
+import { SiteRoles } from 'awayto/core';
+import { useContexts, useComponents, useUtil } from 'awayto/hooks';
 
 const { APP_GROUP_ADMIN, APP_GROUP_ROLES, APP_GROUP_SCHEDULES, APP_GROUP_SERVICES, APP_GROUP_USERS } = SiteRoles;
 
 export function ManageGroupHome(props: IProps): React.JSX.Element {
-  const { groupName, component } = useParams();
 
-  if (!groupName || !component) return <></>;
+  const { component } = useParams();
+  if (!component) return <></>;
+
+  const { setSnack } = useUtil();
+
+  const {
+    group
+  } = useContext(useContexts().GroupContext) as GroupContextType;
 
   const navigate = useNavigate();
 
@@ -29,16 +42,16 @@ export function ManageGroupHome(props: IProps): React.JSX.Element {
     schedules: [APP_GROUP_SCHEDULES],
   }
 
-  const menu = Object.keys(menuRoles).map(comp =>{
+  const menu = Object.keys(menuRoles).map(comp => {
     const selected = comp === component;
-    return groupName && component && <GroupSecure key={`menu_${comp}`} contentGroupRoles={menuRoles[comp]}>
+    return group.name && component && <GroupSecure key={`menu_${comp}`} contentGroupRoles={menuRoles[comp]}>
       <Grid item>
         <Link
           variant="button"
           color={selected ? "secondary" : "primary"}
           sx={{ cursor: 'pointer' }}
           style={selected ? { textDecoration: 'underline' } : undefined}
-          onClick={() => navigate(`/group/${groupName}/manage/${comp}`)}
+          onClick={() => navigate(`/group/${group.name}/manage/${comp}`)}
         >
           {comp}
         </Link>
@@ -65,18 +78,60 @@ export function ManageGroupHome(props: IProps): React.JSX.Element {
       default:
         return;
     }
-  }, [component])
+  }, [component]);
+
+  const copyCode = () => {
+    window.navigator.clipboard.writeText(group.code).catch(console.error);
+    setSnack({ snackType: 'success', snackOn: 'Group code copied to clipboard!' })
+  }
+
+  const groupUrl = `https://${window.location.hostname}/join/${group.code}`;
+
+  const copyUrl = () => {
+    window.navigator.clipboard.writeText(groupUrl).catch(console.error);
+    setSnack({ snackType: 'success', snackOn: 'Invite URL copied to clipboard!' })
+  }
 
   return <>
 
+    <Card sx={{ mb: 2 }}>
+      <CardHeader title={group.displayName} subheader={group.purpose} />
+      <CardContent>
+
+        <Box mb={2}>
+          <Alert sx={{ py: 0 }} icon={false} severity="info">
+            <Typography fontWeight={500}>Group Code:</Typography>
+            <Tooltip title="Copy Group Code">
+              <Link sx={{ cursor: 'pointer' }} onClick={copyCode}>{group.code}</Link>
+
+            </Tooltip>
+          </Alert>
+        </Box>
+
+        <Box mb={2}>
+          <Alert sx={{ py: 0 }} icon={false} severity="info">
+            <Typography fontWeight={500}>Invite Url</Typography>
+            <Tooltip title="Copy Invite URL">
+              <Link sx={{ cursor: 'pointer' }} onClick={copyUrl} variant="body2">{groupUrl}</Link>
+            </Tooltip>
+          </Alert>
+        </Box>
+      </CardContent>
+    </Card>
+
+
+
+
     <Grid container pb={2} spacing={2} justifyContent="flex-start" alignItems="center">
       <Grid item>
-        <Typography variant="button">Controls:</Typography> 
+        <Typography variant="button">Controls:</Typography>
       </Grid>
       {menu}
     </Grid>
 
-    {viewPage}
+    <Suspense>
+      {viewPage}
+    </Suspense>
   </>
 }
 
