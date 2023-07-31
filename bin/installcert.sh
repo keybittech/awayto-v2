6968338c72137377613d87414d72acd602931b89
Joe McCormick <joe.c.mccormick@gmail.com>
2023-07-31 16:58:09 -0700

    cert install changes

--- bin/installcert.sh
+++ bin/installcert.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/expect
 
 # Set environment variables
-set env(EASYRSA_REQ_CN) "$::env(SERVER_NAME)"
-set operator $::env(TAILSCALE_OPERATOR)
-set CA_PASS $::env(CA_PASS)
-set SERVER_NAME $::env(SERVER_NAME)
+set TAILSCALE_OPERATOR $env(TAILSCALE_OPERATOR)
+set CA_PASS $env(CA_PASS)
+set SERVER_NAME $env(SERVER_NAME)
 
-
-set EASYRSA_REQ_ORG $::env(PROJECT_PREFIX)
-set EASYRSA_REQ_EMAIL $::env(ADMIN_EMAIL)
+set EASYRSA_REQ_ORG $env(PROJECT_PREFIX)
+set EASYRSA_REQ_EMAIL $env(ADMIN_EMAIL)
 set EASYRSA_REQ_OU "Internal"
-set EASYRSA_REQ_CN $::env(SERVER_NAME)
+set EASYRSA_REQ_CN $env(SERVER_NAME)
 set EASYRSA_ALGO "ec"
 set EASYRSA_DIGEST "sha512"
 set EASYRSA_BATCH 1
 
 # Spawn the EasyRSA gen-req command
-spawn /home/$operator/easy-rsa/easyrsa gen-req $SERVER_NAME nopass
+spawn /home/$TAILSCALE_OPERATOR/easy-rsa/easyrsa gen-req $SERVER_NAME nopass
+expect eof
 
 # We don't expect any output or interaction here
 
 # Now spawn the sign-req command
-spawn echo "$CA_PASS" | /home/$operator/easy-rsa/easyrsa sign-req server $SERVER_NAME
+spawn /home/$TAILSCALE_OPERATOR/easy-rsa/easyrsa sign-req server $SERVER_NAME
+
+expect "Enter pass phrase for /home/$TAILSCALE_OPERATOR/easy-rsa/pki/private/ca.key"
+send -- "$env(CA_PASS)\r"
 
 # Listen for the confirmation prompt
 expect "Confirm request details:"
