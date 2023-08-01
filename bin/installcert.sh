09d795ca65c07f301f3e9fe0ac94a805e45d1a67
Joe McCormick <joe.c.mccormick@gmail.com>
2023-07-31 17:21:01 -0700

    remove easyrsa env vars during expect

--- bin/installcert.sh
+++ bin/installcert.sh
@@ -5,14 +5,6 @@ set TAILSCALE_OPERATOR $env(TAILSCALE_OPERATOR)
 set CA_PASS $env(CA_PASS)
 set SERVER_NAME $env(SERVER_NAME)
 
-set EASYRSA_REQ_ORG $env(PROJECT_PREFIX)
-set EASYRSA_REQ_EMAIL $env(ADMIN_EMAIL)
-set EASYRSA_REQ_OU "Internal"
-set EASYRSA_REQ_CN $env(SERVER_NAME)
-set EASYRSA_ALGO "ec"
-set EASYRSA_DIGEST "sha512"
-set EASYRSA_BATCH 1
-
 # Spawn the EasyRSA gen-req command
 spawn /home/$TAILSCALE_OPERATOR/easy-rsa/easyrsa gen-req $SERVER_NAME nopass
 expect eof
