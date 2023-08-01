607b7c90446a588fb5f55f482a2a3aec705f34ff
Joe McCormick <joe.c.mccormick@gmail.com>
2023-07-31 21:35:47 -0700

    update cert installer

--- bin/installcert.sh
+++ bin/installcert.sh
@@ -6,5 +6,8 @@ spawn ./easyrsa sign-req server $env(SERVER_NAME)
 expect "Confirm request details:"
 send -- "yes\r"
 
+expect "Enter pass phrase for /home/$env(TAILSCALE_OPERATOR)/easy-rsa/pki/private/ca.key:"
+send -- $env(CA_PASS)"\r"
+
 # Expect end of the program
 expect eof
