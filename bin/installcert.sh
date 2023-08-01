2bf57adf3ac23317e4e70341c780e81ae2276675
Joe McCormick <joe.c.mccormick@gmail.com>
2023-07-31 21:38:10 -0700

    test diff expect syntax

--- bin/installcert.sh
+++ bin/installcert.sh
@@ -7,7 +7,7 @@ expect "Confirm request details:"
 send -- "yes\r"
 
 expect "Enter pass phrase for /home/$env(TAILSCALE_OPERATOR)/easy-rsa/pki/private/ca.key:"
-send -- $env(CA_PASS)"\r"
+send -- "$env(CA_PASS)\r"
 
 # Expect end of the program
 expect eof
