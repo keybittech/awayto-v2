6a045ccb81b06325c3800fba930977539ea0f442
Joe McCormick <joe.c.mccormick@gmail.com>
2023-08-01 04:08:23 -0700

    test basic cert setup

--- api/src/apiserver.ts
+++ api/src/apiserver.ts
@@ -36,13 +36,12 @@ import { setupMiddleware } from './middlewares';
 const app: Express = express();
 
 const key = fs.readFileSync('db_host.key', 'utf-8');
-const cert = fs.readFileSync('db_fullchain.pem', 'utf-8');
-const exitFullchain = fs.readFileSync('exit_fullchain.pem', 'utf-8');
-const caCert = fs.readFileSync('ca.crt', 'utf-8');
+const cert = fs.readFileSync('db_host.crt', 'utf-8');
+const ca = fs.readFileSync('ca.crt', 'utf-8');
 
-https.globalAgent.options.ca = [ ...tls.rootCertificates, cert, exitFullchain, caCert ];
+https.globalAgent.options.ca = [ ca ];
 
-const creds = { key, cert };
+const creds = { key, cert, ca };
 const httpsServer = https.createServer(creds, app)
 
 // const httpServer = http.createServer();
