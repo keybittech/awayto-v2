563fbebc0b39bc87fa8623016eb13d9b5d3033ec
Joe McCormick <joe.c.mccormick@gmail.com>
2023-07-31 18:12:25 -0700

    better api startup logging

--- api/src/middlewares/setupMiddleware.ts
+++ api/src/middlewares/setupMiddleware.ts
@@ -13,9 +13,10 @@ const {
 
 export const setupMiddleware = async (app: Express) => {
   // Configure for reverse proxy
+  console.log('Setting trust proxy');
   app.set('trust proxy', true);
 
-
+  console.log('Setting cookie session secret');
   // Store keycloak token in session
   app.use(cookieSession({
     name: 'primary_session',
@@ -25,11 +26,14 @@ export const setupMiddleware = async (app: Express) => {
     httpOnly: true
   }));
 
+  console.log('Setting cookie parser');
   // Enable cookie parsing to read keycloak token
   app.use(cookieParser());
 
+  console.log('Setting express JSON');
   app.use(express.json());
 
+  console.log('Setting passport session regen workaround');
   // Fix for passport/express cookie issue
   app.use((req: Request, res: Response, next: NextFunction) => {
     if (req.session && !req.session.regenerate) {
@@ -47,8 +51,10 @@ export const setupMiddleware = async (app: Express) => {
     next()
   });
 
+  console.log('Setting passport initialization');
   app.use(passport.initialize());
 
+  console.log('Setting passport session auth');
   app.use(passport.authenticate('session'));
 
   const strategyResponder: StrategyVerifyCallbackUserInfo<StrategyUser> = (tokenSet, userInfo, done) => {
@@ -56,16 +62,20 @@ export const setupMiddleware = async (app: Express) => {
     return done(null, { sub });
   }
 
+  console.log('Awaiting issuer discovery');
   const keycloakIssuer = await Issuer.discover(keycloakDiscoveryUrl);
 
+  console.log('Setting OIDC strategy');
   passport.use('oidc', new Strategy<StrategyUser>({
     client: new keycloakIssuer.Client(keycloakClientConfiguration)
   }, strategyResponder));
 
+  console.log('Setting user serialization');
   passport.serializeUser(function (user, done) {
     done(null, user);
   });
 
+  console.log('Setting user de-serialization');
   passport.deserializeUser<Express.User>(function (user, done) {
     done(null, user);
   });
