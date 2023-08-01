33f37eabcaf254c9994151f954e7350c3370ac90
Joe McCormick <joe.c.mccormick@gmail.com>
2023-08-01 04:41:29 -0700

    test db_fullchain

--- api/src/apiserver.ts
+++ api/src/apiserver.ts
@@ -36,7 +36,7 @@ import { setupMiddleware } from './middlewares';
 const app: Express = express();
 
 const key = fs.readFileSync('db_host.key', 'utf-8');
-const cert = fs.readFileSync('db_host.crt', 'utf-8');
+const cert = fs.readFileSync('db_fullchain.pem', 'utf-8');
 const ca = fs.readFileSync('ca.crt', 'utf-8');
 
 https.globalAgent.options.ca = [ ca ];
